561217ee12e8b63f34bdc9e21c3e67bc813a293b
Mads Nielsen <mads130f@edu.easj.dk>
2020-01-25 12:57:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/apitraining/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.apitraining;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mainText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainText = findViewById(R.id.mainText);
+
+        //
+        RequestQueue queue = 
+
+        Volley
+
+
+
+    }
+}
