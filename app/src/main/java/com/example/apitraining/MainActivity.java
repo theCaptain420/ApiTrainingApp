841e01337605646b04ce1f6514b3d4c4ed94d5d5
thecaptain420 <mads.nielsen@hotmail.com>
2020-01-25 13:07:10 +0100

    Working project to get thorapp data

--- app/src/main/java/com/example/apitraining/MainActivity.java
+++ app/src/main/java/com/example/apitraining/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,12 +23,26 @@ public class MainActivity extends AppCompatActivity {
 
         mainText = findViewById(R.id.mainText);
 
-        //
-        RequestQueue queue = 
+        //set up request queue
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://thormasters.com/TMAppData.json";
 
-        Volley
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //displayText
+                        mainText.setText("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mainText.setText("Didnt work");
+            }
+        }
+        );
 
-
+        queue.add(stringRequest);
 
     }
 }
